70135c09d2840d5b60d9b363b7d0f1812ba76edc
AutiCodes <prive@auticodes.nl>
2024-11-16 16:47:02 +0100

    Added default error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+@extends('home::layouts.master')
+
+@section('title', 'Fout 404')
+
+@section('content')
+  <div class="container d-flex align-items-center justify-content-center min-vh-100">
+    <h1>404</h1>
+  </div>
+@stop
